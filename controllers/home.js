324b192e8674922f7f0db35f6cacb5c18cb5228f
LowryTang <27156747@qq.com>
2015-03-13 20:14:11 +0800

    整体架构,闪购页面

--- controllers/home.js
+++ controllers/home.js
@@ -3,9 +3,10 @@ var Product = require('../models/product.js');
 var home = {
   index: function (req, res, next) {
     Product.findAll().then(function (result) {
-      res.render('index', {
+      console.log(result[0].sellPrice);
+      res.render('site/index', {
         title: "test",
-        data: result
+        products: result
       });
     })
   }
--- models/product.js
+++ models/product.js
@@ -1,11 +1,28 @@
+var Sequelize = require('sequelize');
 var sequelize = require('./index');
 
 var prefix = require('../config/config').db.prefix;
 var tableName = prefix + 'goods';
 
-var Product = sequelize.define(tableName, {
+var arrtibutes = {
+  name: {
+    type: Sequelize.STRING
+  },
+  sellPrice: {
+    type: Sequelize.DECIMAL(15,2),
+    field: "sell_price"
+  },
+  img: {
+    type: Sequelize.STRING
+  },
+  isDel: {
+    type: Sequelize.BOOLEAN,
+    field: "is_del"
+  },
 
-}, {
+};
+
+var Product = sequelize.define(tableName, arrtibutes, {
   timestamps: false
 });
 
